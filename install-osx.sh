0c4a8ff828c78e80eba082181e1591ecacca4141
Guilherme Otranto <guilherme.otranto@tagview.com.br>
2020-02-19 09:53:38 -0300

    install for osx

---
+++ install-osx.sh
@@ -0,0 +1,8 @@
+#!zsh
+
+node ./compile-xml.js
+
+cp lodash.mixxx.js ~/Library/Application\ Support/Mixxx/controllers
+cp midi-components-0.0.js ~/Library/Application\ Support/Mixxx/controllers
+cp Pioneer-DDJ-SB3-scripts.js ~/Library/Application\ Support/Mixxx/controllers
+cp Pioneer-DDJ-SB3.midi.xml ~/Library/Application\ Support/Mixxx/controllers
