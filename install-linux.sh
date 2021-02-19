c696452970004777b7d6e22c12799447adf8c8e1
Júlia Otranto Aulicino <julia.otranto@outlook.com>
2021-02-19 12:04:54 -0300

    Fix install osx/linux scripts

--- install-linux.sh
+++ install-linux.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-cp lodash.mixxx.js ~/.mixxx/controllers
-cp midi-components-0.0.js ~/.mixxx/controllers
-cp Pioneer-DDJ-SB3-scripts.js ~/.mixxx/controllers
-cp Pioneer-DDJ-SB3.midi.xml ~/.mixxx/controllers
+MIXXX_CONTROLLER_DIR="$HOME/.mixxx/controllers";
+
+cp lodash.mixxx.js "$MIXXX_CONTROLLER_DIR/lodash.mixxx.js";
+cp midi-components-0.0.js "$MIXXX_CONTROLLER_DIR/midi-components-0.0.js";
+cp Pioneer-DDJ-SB3-scripts.js "$MIXXX_CONTROLLER_DIR/Pioneer-DDJ-SB3-scripts.js";
+cp Pioneer-DDJ-SB3.midi.xml "$MIXXX_CONTROLLER_DIR/Pioneer-DDJ-SB3.midi.xml";
+
+echo "Update current mapping to new version?";
+echo "Changes to the mapping will be lost.";
+
+read -p "y/N: " -n1 override;
+echo '';
+
+if [ "$override" = 'y' ]
+then
+  echo "Overrinding....";
+
+  CURR_DIR=$(pwd);
+
+  cd "$MIXXX_CONTROLLER_DIR";
+
+  for FILE in *;
+  do
+    if [[ "$FILE" == *"DDJ-SB3"* ]] && [[ "$FILE" == *".xml" ]];
+    then
+      cp "$CURR_DIR/Pioneer-DDJ-SB3.midi.xml" "$FILE";
+    fi;
+  done
+
+  cd $CURR_DIR;
+fi
--- install-osx.sh
+++ install-osx.sh
@@ -1,6 +1,34 @@
 #!zsh
 
-cp lodash.mixxx.js ~/Library/Application\ Support/Mixxx/controllers
-cp midi-components-0.0.js ~/Library/Application\ Support/Mixxx/controllers
-cp Pioneer-DDJ-SB3-scripts.js ~/Library/Application\ Support/Mixxx/controllers
-cp Pioneer-DDJ-SB3.midi.xml ~/Library/Application\ Support/Mixxx/controllers
+MIXXX_CONTROLLER_DIR="$HOME/Library/Application Support/Mixxx/controllers";
+
+cp lodash.mixxx.js "$MIXXX_CONTROLLER_DIR/lodash.mixxx.js";
+cp midi-components-0.0.js "$MIXXX_CONTROLLER_DIR/midi-components-0.0.js";
+cp Pioneer-DDJ-SB3-scripts.js "$MIXXX_CONTROLLER_DIR/Pioneer-DDJ-SB3-scripts.js";
+cp Pioneer-DDJ-SB3.midi.xml "$MIXXX_CONTROLLER_DIR/Pioneer-DDJ-SB3.midi.xml";
+
+echo "Update current mapping to new version?";
+echo "Changes to the mapping will be lost.";
+
+vared -p "y/N: " -c override;
+echo '';
+
+if [ "$override" = 'y' ]
+then
+  echo 'Overriding';
+
+  CURR_DIR=$(pwd);
+
+  cd "$MIXXX_CONTROLLER_DIR";
+
+  for FILE in *;
+  do
+    if [[ "$FILE" == *"DDJ-SB3"* ]] && [[ "$FILE" == *".xml" ]];
+    then
+      cp "$CURR_DIR/Pioneer-DDJ-SB3.midi.xml" "$FILE";
+    fi;
+  done
+
+  cd $CURR_DIR;
+fi
+
